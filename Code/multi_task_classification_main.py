1631c1f9358f6153155a19ff5d6ef1711c8c1f89
Lisa-Marie Rolli <lisa-marie.rolli@bioinf.uni-sb.de>
2024-11-27 11:50:58 +0100

    feature selection enabled

--- Code/multi_task_classification_main.py
+++ Code/multi_task_classification_main.py
@@ -4,6 +4,7 @@ import random
 from sklearn.metrics import matthews_corrcoef
 import numpy as np
 import copy
+from sklearn.model_selection import StratifiedKFold
 
 
 def main():
@@ -26,6 +27,7 @@ def main():
                                   100, 200, 300, 400, 500], max_depth_range=[10, 20, 30], min_samples_leaf_range=[5, 10, 15])
     train_indices = {}
     test_indices = {}
+    cv_folds = []
     for i in range(8):
         responses_i = responses.iloc[i, :].dropna()
         profiles_i = profiles_cid.loc[list(responses_i.index), :]
@@ -49,12 +51,27 @@ def main():
         responses_test.to_csv(
             f'../Data/Output/{receptor_name}_y_test.csv', sep='\t')
 
+        skf = StratifiedKFold(
+            n_splits=5, shuffle=True, random_state=42)
+        folds = skf.split(X=profiles_train, y=responses_train)
+        cv_folds.append(folds)
+
+    with open('../Data/Output/feature_selection_files_MRMR.txt', 'r') as feature_selection_files:
+        files = feature_selection_files.read().splitlines()
+        features_for_training = []
+        for file in files:
+            fs_dataframe = pd.read_csv(file, sep='\t')
+            features = fs_dataframe.iloc[:, 1].values
+            top_50_features = features[:100]
+            features_for_training.append(top_50_features)
+
     mfe.cross_validate_and_fit_forests(
-        X_train=copy.deepcopy(profiles_cid), y_train=responses, train_indices=train_indices)
+        X_train=copy.deepcopy(profiles_cid), y_train=responses, train_indices=train_indices, train_feature_list=features_for_training,
+        cv_folds=cv_folds)
     predictions_test = mfe.predict(
-        X_test=profiles_cid, test_indices=test_indices)
+        X_test=profiles_cid, test_indices=test_indices, train_feature_list=features_for_training)
     predictions_train = mfe.predict(
-        X_test=profiles_cid, test_indices=train_indices)
+        X_test=profiles_cid, test_indices=train_indices, train_feature_list=features_for_training)
     for i, pred in enumerate(predictions_test):
         act = responses.iloc[i, :]
         act_test = act[test_indices[i]]
